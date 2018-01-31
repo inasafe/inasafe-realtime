2b73b970a48b99cf5905b6829da7fad85439ed06
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-01-31 23:11:35 +0700

    Include production grade container

--- deployment/production/docker/realtime/docker-entrypoint.sh
+++ deployment/production/docker/realtime/docker-entrypoint.sh
@@ -9,6 +9,4 @@ if [ $# -eq 2 ] && [ $1 = "prod" ] && [ $2 = "inasafe-realtime-worker" ]; then
     /usr/local/bin/celery -A realtime.celery_app worker -l info -Q inasafe-realtime -n inasafe-realtime.%h
 elif [ $# -eq 2 ] && [ $1 = "prod" ] && [ $2 = "inasafe-realtime-monitor" ]; then
 	python realtime/earthquake/notify_new_shake.py /home/realtime/shakemaps
-elif [ $# -eq 1 ] && [ $1 = "dev" ]; then
-	/usr/sbin/sshd -D
 fi
