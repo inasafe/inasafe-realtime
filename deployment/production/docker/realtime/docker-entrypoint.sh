5192eac10ad4fe2c10d824e206c1ac1a124ec9cb
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-03-08 16:03:28 +0700

    Include shakemaps directory in shake monitor arguments

--- deployment/production/docker/realtime/docker-entrypoint.sh
+++ deployment/production/docker/realtime/docker-entrypoint.sh
@@ -13,5 +13,5 @@ cp -n /home/app/realtime/celeryconfig_sample.py /home/app/realtime/celeryconfig.
 if [ $# -eq 2 ] && [ $1 = "prod" ] && [ $2 = "inasafe-realtime-worker" ]; then
     /usr/local/bin/celery -A realtime.celery_app worker -l info -Q inasafe-realtime -n inasafe-realtime.%h
 elif [ $# -eq 2 ] && [ $1 = "prod" ] && [ $2 = "inasafe-realtime-monitor" ]; then
-	python realtime/earthquake/notify_new_shake.py /home/realtime/shakemaps
+	python realtime/earthquake/notify_new_shake.py ${SHAKEMAPS_DIR}
 fi
