671fb8d539e363905f4a8356b6505ef1a59e6e78
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-05-24 06:10:56 +0700

    Add Celery Late ACK settings

--- src/realtime/celeryconfig_sample.py
+++ src/realtime/celeryconfig_sample.py
@@ -52,5 +52,10 @@ task_serializer = 'pickle'
 accept_content = {'pickle'}
 result_serializer = 'pickle'
 
+
+# Late ACK settings
+task_acks_late = True
+task_reject_on_worker_lost = True
+
 task_always_eager = ast.literal_eval(os.environ.get(
        'TASK_ALWAYS_EAGER', 'False'))
